05fff872a6f982db56ddd56c2d07d28cd7e45c1e
Jisoo.R <jisuryu0527@gmail.com>
2024-10-11 23:22:10 +0900

    Initial project setup

---
+++ src/main/java/com/sparta/assignment_todo_developed/AssignmentTodoDevelopedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.assignment_todo_developed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTodoDevelopedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentTodoDevelopedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/assignment_todo_developed/AssignmentTodoDevelopedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.assignment_todo_developed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentTodoDevelopedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
