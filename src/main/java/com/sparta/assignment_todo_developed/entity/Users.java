3236b2f80e17174aa8e144d26b90b1962b6b8a1b
Jisoo.R <jisuryu0527@gmail.com>
2024-10-30 22:59:43 +0900

    feat: Add JwtToken - refreshToken
    * started -> add class files

---
+++ src/main/java/com/sparta/assignment_todo_developed/entity/Users.java
@@ -0,0 +1,51 @@
+package com.sparta.assignment_todo_developed.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Getter
+public class Users implements UserDetails {
+
+    private final Member member;
+
+    public Users(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
